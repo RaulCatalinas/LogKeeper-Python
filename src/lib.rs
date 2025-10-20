bbd0d42edddeaf5f13feb70b437c3edd2bfda91f
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2025-10-20 14:53:40 +0200

    chore: Files lib.rs, pyproject.toml, Cargo.toml, uv.lock updated

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod pybindings;
 use pyo3::prelude::*;
 
 #[pymodule]
-fn py_logkeeper(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn logkeeper_py(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<crate::pybindings::LogKeeper>()?;
     Ok(())
 }
